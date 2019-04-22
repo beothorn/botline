885987e1712d23ac7dd5b77216796094911dd47d
Carol <carolinecollaco@gmail.com>
2019-04-22 02:02:53 +0200

    Adapting to sqlite3

--- persistence.py
+++ persistence.py
@@ -9,7 +9,7 @@ def execute(db_file, query, values):
     conn = sqlite3.connect(db_file)
     cur = conn.cursor()
     query_log = "QUERY => "+query
-    logger.info(query_log % values)
+    #logger.info(query_log % values)
     cur.execute(query, values)
     conn.commit()
 
@@ -33,12 +33,12 @@ def create_db(db_file):
     connection.commit()
 
 def record_msg(db_file, user_id, message):
-    execute(db_file, """INSERT INTO msg_received (user_id, message, created_at) VALUES (%s, %s, current_date)""", (user_id, message))
+    execute(db_file, """INSERT INTO msg_received (user_id, message, created_at) VALUES (?, ?, CURRENT_TIMESTAMP)""", (user_id, message))
 
 def get_allowed_ids(db_file):
     connection = sqlite3.connect(db_file)
     return connection.execute('SELECT user_id FROM allowed_ids').fetchall()
 
 def add_allowed_id(db_file, user_id):
-    execute(db_file, """INSERT INTO allowed_ids (user_id) VALUES (%s)""", (user_id))
+    execute(db_file, """INSERT INTO allowed_ids (user_id) VALUES (?)""", (user_id,))
 
